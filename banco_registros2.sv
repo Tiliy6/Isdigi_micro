7cb84a0142a076abb1648ff3f139e64ffc23480c
ulisesgarrod <ugrtrabajos@gmail.com>
2025-11-18 12:19:26 +0100

    Create banco_registros2.sv

---
+++ banco_registros2.sv
@@ -0,0 +1,20 @@
+module banco_registros2(CLK, readReg1, readReg2, writeReg, writeData, readData1, readData2, RegWrite);
+input [4:0] readReg1, readReg2, writeReg;
+input CLK, RegWrite; //RegWrite enable de escritura
+input [31:0] writeData;
+output [31:0] readData1, readData2;
+logic [31:0][31:0]registro; //x0=registro[0],x1=registro[1], ect
+
+
+always_ff @(posedge CLK)
+	if(RegWrite)
+		registro[writeReg] = writeData;
+			
+always_comb
+	readData1 = registro[readReg1];
+	
+always_comb
+	readData2 = registro[readReg2];
+
+	
+endmodule
