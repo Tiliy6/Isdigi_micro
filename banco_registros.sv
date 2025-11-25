a56da4d85ab9b8ed0da042d5982060b1a63d7b19
ulisesgarrod <ugrtrabajos@gmail.com>
2025-11-25 12:15:45 +0100

    Update banco_registros.sv

--- banco_registros.sv
+++ banco_registros.sv
@@ -2,8 +2,8 @@ module banco_registros(CLK, readReg1, readReg2, writeReg, writeData, readData1, 
 input [4:0] readReg1, readReg2, writeReg;
 input CLK, RegWrite; //RegWrite enable de escritura
 input [31:0] writeData;
-output [31:0] readData1, readData2;
-logic [31:0][31:0]registro; //x0=registro[0],x1=registro[1], ect
+output logic [31:0] readData1, readData2;
+logic [31:0] registro [31:0]; //x0=registro[0],x1=registro[1], ect
 
 
 always_ff @(posedge CLK)
