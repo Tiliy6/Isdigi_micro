ccb33017e873d1241e1a758149777f19670ab286
remonsot <remonsot@teleco.upv.es>
2025-11-26 11:06:42 +0100

    RAM.sv

--- RAM.sv
+++ RAM.sv
@@ -1,19 +1,21 @@
-// RAM parametrizable: lectura asíncrona, escritura síncrona
+// RAM parametrizable: lectura asíncrona, escritura síncrona, enable de lectura
 module RAM #(
     parameter ANCHO = 32,              // bits por palabra
     parameter LARGO = 1024,            // número de posiciones
     parameter INIT_FILE = ""           // fichero de inicialización
 )(
     input                        clk,
-    input                        write_enable,   // habilitación de escritura
-    input  [$clog2(LARGO)-1:0]   addr,       // dirección
-    input  [ANCHO-1:0]           din,        // datos de entrada
-    output [ANCHO-1:0]           dout        // datos de salida
+    input                        write_enable,     // habilitación de escritura
+    input                        read_enable,      // habilitación de lectura
+    input  [$clog2(LARGO)-1:0]   addr,             // dirección
+    input  [ANCHO-1:0]           din,              // datos de entrada
+    output [ANCHO-1:0]           dout              // datos de salida
 );
+
     // Memoria: LARGO x ANCHO
     reg [ANCHO-1:0] mem [0:LARGO-1];
 
-    
+    // Inicialización opcional
     initial begin
         if (INIT_FILE != "") begin
             $readmemh(INIT_FILE, mem);
@@ -27,7 +29,9 @@ module RAM #(
         end
     end
 
-    // Lectura asíncrona
-    assign dout = mem[addr];
+    // Lectura asíncrona CON habilitación
+    assign dout = (read_enable) ? mem[addr] : {ANCHO{1'b0}};
 
 endmodule
+
+
