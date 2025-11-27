b2981845cdcb03137dd223eb1c86c78396a45e89
ulisesgarrod <ugrtrabajos@gmail.com>
2025-11-27 21:02:16 +0100

    Update tb_RAM.sv

--- verificacion/tb_RAM.sv
+++ verificacion/tb_RAM.sv
@@ -1,70 +1,69 @@
 `timescale 1ns/1ps
+
 	module tb_RAM();
+	localparam T = 20;
 	
 	parameter ANCHO = 32;        
-   	parameter LARGO = 1024;
+   parameter LARGO = 1024;
 	
-	logic 		CLK, write_enable;
+	logic 		CLK, write_enable, read_enable;
 	logic [$clog2(LARGO)-1:0] addr;
 	logic [ANCHO-1:0]  din,dout;
 	
 	
-	RAM (
-	.clk(CLK),
+	RAM duv(
+	.CLK(CLK),
 	.write_enable(write_enable),
+	.read_enable(read_enable),
 	.addr(addr),
 	.din(din),
 	.dout(dout)
 	);
 	
 	
-	always
-	begin
-		CLK = 1'b0;
-		CLK = #50 1'b1;
-		#50;
-	end
-	
+	 always
+	 begin
+	 #(T/2) CLK = ~CLK;
+	 end
 	
 	
 	task escritura_correcta;
 		begin
 			write_enable = 1'b1;
-			addr  = 5'b01101;
-			din = 4'hA234;
-			repeat(2) @(posedge CLK);
-			addr = 5'b01101;
-			@(dout);
-			assert (dout == 4'hA234) else $error("No escribe correctamente");
-			
+			read_enable = 1'b1;
+			addr  = 10'd13;
+			din = 32'hA234;
+			repeat(2) @(negedge CLK);
+			addr = 10'd13;
+			@(negedge CLK);
+			assert (dout == 32'hA234) else $error("No escribe correctamente");
 		end
 	endtask
 	
 	task lectura_escritura_simultanea;
 		begin
 			write_enable = 1'b1;
-			addr = 5'b10000;
-			din = 4'h1234;
-			@(posedge CLK);
+			read_enable = 1'b1;
+			addr = 10'd16;
+			din = 32'h1234;
+			@(negedge CLK);
 			write_enable=1'b0;
-			@(dout);
-			assert (dout == 4'h1234) else $error("La lectura no es simultanea");
+			@(negedge CLK);
+			assert (dout == 32'h1234) else $error("La lectura no es simultanea");
 		end
 	endtask
 			
 			
 	initial
 	begin
+	CLK = 0;
 	escritura_correcta();
 	@(negedge CLK);
 	lectura_escritura_simultanea();
+	@(negedge CLK);
+	$display("Simulacion finalizada");
+	$stop;
 	end
 	
-	
-			
-			
-	
 endmodule
-			
-			
-			
+		
