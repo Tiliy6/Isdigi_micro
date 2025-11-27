ebd4c4af6523869a261c9ceb9c2bfe58e7289f11
ulisesgarrod <ugrtrabajos@gmail.com>
2025-11-27 21:47:50 +0100

    Update RAM.sv

--- RAM.sv
+++ RAM.sv
@@ -1,13 +1,13 @@
-// RAM parametrizable: lectura asíncrona, escritura síncrona, enable de lectura
+// RAM parametrizable: lectura asincrona, escritura sincrona, enable de lectura
 module RAM #(
     parameter ANCHO = 32,              // bits por palabra
-    parameter LARGO = 1024,            // número de posiciones
-    parameter INIT_FILE = ""           // fichero de inicialización
+    parameter LARGO = 1024,            // numero de posiciones
+    parameter INIT_FILE = ""           // fichero de inicializacion
 )(
     input                        CLK,
-    input                        write_enable,     // habilitación de escritura
-    input                        read_enable,      // habilitación de lectura
-    input  [$clog2(LARGO)-1:0]   addr,             // dirección
+    input                        write_enable,     // habilitacion de escritura
+    input                        read_enable,      // habilitacion de lectura
+    input  [$clog2(LARGO)-1:0]   addr,             // direccion
     input  [ANCHO-1:0]           din,              // datos de entrada
     output [ANCHO-1:0]           dout              // datos de salida
 );
@@ -15,21 +15,21 @@ module RAM #(
     // Memoria: LARGO x ANCHO
     reg [ANCHO-1:0] mem [0:LARGO-1];
 
-    // Inicialización opcional
+    // Inicializacion opcional
     initial begin
         if (INIT_FILE != "") begin
             $readmemh(INIT_FILE, mem);
         end
     end
 
-    // Escritura síncrona
+    // Escritura sincrona
     always @(posedge CLK) begin
         if (write_enable) begin
             mem[addr] <= din;
         end
     end
 
-    // Lectura asíncrona CON habilitación
+    // Lectura asincrona CON habilitacion
     assign dout = (read_enable) ? mem[addr] : {ANCHO{1'b0}};
 
 endmodule
