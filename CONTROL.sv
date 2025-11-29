58466a3f65b59908caf6b3491ed0878b87864d0b
EmilioGarcia777 <bolinaranja350@gmail.com>
2025-11-29 20:27:35 +0100

    Update CONTROL.sv

--- CONTROL.sv
+++ CONTROL.sv
@@ -3,11 +3,11 @@ module CONTROL(
     output logic        Branch,
     output logic        MemRead,
     output logic        MemtoReg,
-    output logic  [1:0] ALUOp,
+    output logic  [2:0] ALUOp,
     output logic        MemWrite,
     output logic        ALUSrc,
     output logic        RegWrite,
-	 output logic        AuipcLui
+	 output logic  [1:0] AuipcLui
 );
 
 
@@ -18,22 +18,22 @@ module CONTROL(
         Branch    = 0;
         MemRead   = 0;
         MemtoReg  = 0;
-        ALUOp     = 2'b00;
+        ALUOp     = 3'b000;
         MemWrite  = 0;
         ALUSrc    = 0;
         RegWrite  = 0;
-		  AuipcLui  = 0;
+		  AuipcLui  = 2'b11;
         case (opcode)
 
             // R-format
             7'b01100: begin
-                ALUOp     = 2'b00;
+                ALUOp     = 3'b000;
                 RegWrite  = 1;
             end
 
             // I-format
             7'b00100: begin
-                ALUOp     = 2'b11;
+                ALUOp     = 3'b011;
                 ALUSrc    = 1;
                 RegWrite  = 1;
             end
@@ -43,7 +43,7 @@ module CONTROL(
                 MemRead   = 1;
                 MemtoReg  = 1;
                 ALUSrc    = 1;
-                ALUOp     = 2'b10;
+                ALUOp     = 3'b010;
                 RegWrite  = 1;
             end
 
@@ -51,26 +51,41 @@ module CONTROL(
             7'b01000: begin
                 MemWrite  = 1;
                 ALUSrc    = 1;
-                ALUOp     = 2'b10;
+                ALUOp     = 3'b010;
             end
 
             // BEQ
             7'b11000: begin
                 Branch    = 1;
-                ALUOp     = 2'b01;
+                ALUOp     = 3'b001;
             end
 				
 				// LUI
 				7'b01101: begin
 					 RegWrite  = 1;
-					 AuipcLui  = 1;
+					 ALUOp     = 3'b100;
+					 ALUSrc    = 1;
+					 AuipcLui  = 2'b01;//Esto equivale a que el MUX saque un 0
+					 
 				end
 
 				// AUIPC
 				7'b00101: begin
 					 RegWrite  = 1;
-					 AuipcLui  = 1;
+					 ALUOp     = 3'b100; 
+					 ALUSrc    = 1;
+					 AuipcLui  = 2'b00;//Esto equivale a que el MUX saque el PC
 				end
+			default: begin
+				Branch    = 0;
+				MemRead   = 0;
+				MemtoReg  = 0;
+				ALUOp     = 3'b000;
+				MemWrite  = 0;
+				ALUSrc    = 0;
+				RegWrite  = 0;
+				AuipcLui  = 2'b11;
+			end
 
 
         endcase
