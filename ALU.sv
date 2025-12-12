f5480813ead0a88f7c6bd3658367521b3e9a5f4e
ooucelm <oussamaouchtoubane12@gmail.com>
2025-12-12 16:21:10 +0100

    Update ALU.sv
    Añado entrada sel, que selecciona si B es signed o unsigned.

--- ALU.sv
+++ ALU.sv
@@ -1,51 +1,51 @@
 module ALU (
     input logic	 [31:0] A,
-    input logic      [31:0] B,
+    input logic signed  [31:0] B,
     input logic     [3:0]  ALU_control,
+	input logic sel, //seleccion por si necesitamos operar con un inmediato negativo
     output logic [31:0] ALU_result,
     output logic          zero
+
 );
 
-    always @(*) begin
-        case (ALU_control)
+	always @(*) begin
+		case (ALU_control)
 
-            4'b0000: ALU_result = A + B;                 // ADD
-            4'b0001: ALU_result = A - B;                 // SUB
-            4'b0010: ALU_result = A & B;                 // AND
-            4'b0011: ALU_result = A | B;                 // OR
-            4'b0100: ALU_result = A ^ B;                 // XOR
+			4'b0000: ALU_result = (!sel) ? (A + $unsigned(B)) : (A + $signed(B));             // ADD o ADDi
+			4'b0001: ALU_result = A - $unsigned(B) ;             // SUB 
+			4'b0010: ALU_result = (!sel) ? (A & $unsigned(B)) : (A & $signed(B));             // AND o ANDi
+			4'b0011: ALU_result = (!sel) ? (A | $unsigned(B)) : (A | $signed(B));             // OR o ORi
+			4'b0100: ALU_result = (!sel) ? (A ^ $unsigned(B)) : (A ^ $signed(B));             // XOR o XORi
 
-            // SHIFT LEFT LOGIC
-            4'b0101: ALU_result = A << B[4:0];           // SLL
+			// SHIFT LEFT LOGIC
+			4'b0101: ALU_result = A << $unsigned(B[4:0]);        // SLL
 
-            // SHIFT RIGHT LOGIC
-            4'b0110: ALU_result = A >> B[4:0];           // SRL
+			// SHIFT RIGHT LOGIC
+			4'b0110: ALU_result = A >> $unsigned(B[4:0]);        // SRL
 
-            // SHIFT RIGHT ARITHMETIC (CON SIGNO)
-            4'b0111: ALU_result = $signed(A) >>> B[4:0]; // SRA
+			// SHIFT RIGHT ARITHMETIC (CON SIGNO)
+			// Nota: A debe ser tratado como signed para el shift
+			4'b0111: ALU_result = $signed(A) >>> $unsigned(B[4:0]); // SRA 
 
-            // LESS THAN (unsigned)
-			4'b1000: ALU_result = (A < B) ? 32'd1 : 32'd0;   // SLTU  --> BLTU
+			// LESS THAN (unsigned) - SLTU / BLTU
+			4'b1000: ALU_result = (!sel) ? ((A < $unsigned(B)) ? 32'd1 : 32'd0) : 
+										((A < $signed(B)) ? 32'd1 : 32'd0);
 
-            // LESS THAN (signed)
-			4'b1001: ALU_result = ($signed(A) < $signed(B)) ? 32'd1 : 32'd0; // SLT --> BLT
+			// LESS THAN (signed) - SLT / BLT
+			4'b1001: ALU_result = ($signed(A) < $signed(B)) ? 32'd1 : 32'd0; 
 				
-				//BNE
-				4'b1010: ALU_result = A - B;
-
-				 // LESS THAN (unsigned)
-				4'b1011: ALU_result = (A < B) ? 32'd1 : 32'd0;   // SLTU  --> BGEU
-	
-	            // LESS THAN (signed)
-				4'b1100: ALU_result = ($signed(A) < $signed(B)) ? 32'd1 : 32'd0; // SLT --> BGE
-	
-								
+			// BNE (Comparación)
+			4'b1010: ALU_result = A - $unsigned(B);
 				
+			// LESS THAN (unsigned) - SLTU / BGEU
+			4'b1011: ALU_result = (A < $unsigned(B)) ? 32'd1 : 32'd0; 
 
-            default: ALU_result = 32'd0;
-
-        endcase
-    end
+			// LESS THAN (signed) - SLT / BGE
+			4'b1100: ALU_result = ($signed(A) < $signed(B)) ? 32'd1 : 32'd0; 
+				
+			default: ALU_result = 32'd0;
+		endcase
+	end
 
     assign zero =
     (ALU_control == 4'b0001) ? (ALU_result == 0) :      // BEQ  → igual
@@ -55,6 +55,4 @@ module ALU (
 	(ALU_control == 4'b1011) ? (ALU_result == 0) :      // BGE  → SLT = 0
 	(ALU_control == 4'b1100) ? (ALU_result == 0) :      // BGEU → SLTU = 0
     1'b0;                                               // resto (no branch)
-
 endmodule
-
