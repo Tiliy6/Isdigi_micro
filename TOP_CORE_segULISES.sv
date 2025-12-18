fb8a4476f3e9b2b43d2af5a6dbc4fc87fb636358
ulisesgarrod <ugrtrabajos@gmail.com>
2025-12-18 22:28:30 +0100

    Update TOP_CORE_segULISES.sv
    SIN REVISAR

--- TOP_CORE_segULISES.sv
+++ TOP_CORE_segULISES.sv
@@ -1,10 +1,10 @@
-module TOP_CORE(instr_IF, datareg_wr, CLOCK, RST_n, PC_IF, ena_wr, ena_rd, alu_out_ext, MemtoReg_sig, dataram_wr);
+module TOP_CORE_seg(instr, CLOCK, RST_n, dataram_rd, PC, ena_wr, ena_rd, alu_out_ext, dataram_wr);
 
-input [31:0] instr_IF;
+input [31:0] instr;
 input [31:0] datareg_wr; //salida mux memtoreg
 input RST_n, CLOCK;
 
-output logic [31:0] PC_IF;
+output logic [31:0] PC;
 output logic ena_wr, ena_rd; //entrada habilita ram lectura/escritura
 output logic MemtoReg_sig; //habilita mux de ram
 output logic [31:0] alu_out_ext; //entrada ram
@@ -19,28 +19,31 @@ logic [3:0] instruction_bits_sig, ALU_operation;
 
 always_ff @(posedge CLOCK or negedge RST_n)
 	if (!RST_n)
-		PC_IF <= '0;
+		PC <= '0;
 	else 
-		PC_IF <= PC_siguiente;
+		PC <= PC_siguiente;
+
+
+assign PC_siguiente = (PCSrc) ? (PC_inm_MEM) : //Jalr_sig ? alu_out_ext : (PC + 4); ******************REVISAR******************
 
 
 //IF/ID
 always @(posedge CLK)		
-	PC_ID = PC_IF;
-	instr_ID = instr_IF;
+	PC_ID = PC;
+	instr_ID = instr;
 
 
 banco_registros banco_registros_inst
 (
-	.CLK(CLOCK),	// input  CLK_sig
-	.RST_n(RST_n),	// input  RST_n_sig
+	.CLK(CLOCK),						// input  CLK_sig
+	.RST_n(RST_n),						// input  RST_n_sig
 	.readReg1(instr_ID[19:15]),	// input [4:0] readReg1_sig
 	.readReg2(instr_ID[24:20]),	// input [4:0] readReg2_sig
-	.writeReg(instr_ID[11:7]),	// input [4:0] writeReg_sig
-	.writeData(datareg_wr),	// salida mux MemtoReg						
-	.readData1(regis_A),	// output [31:0] readData1_sig
-	.readData2(regis_B),	// output [31:0] readData2_sig
-	.RegWrite(RegWrite_sig) 	// input  RegWrite_sig
+	.writeReg(instr_WB[11:7]),		// input [4:0] writeReg_sig
+	.writeData(datareg_wr),			// salida mux MemtoReg						
+	.readData1(readData1_ID),		// output [31:0] readData1_sig
+	.readData2(readData2_ID),		// output [31:0] readData2_sig
+	.RegWrite(RegWrite_sig) 		// input  RegWrite_sig
 );
 
 
@@ -54,18 +57,19 @@ Inm_Gen Inm_Gen_inst
 CONTROL CONTROL_inst
 (
 	.instruction(instr_ID),	// input [31:0] instruction_sig
-	.Branch(Branch_ID),	// output  Branch_sig
+	.Branch(Branch_ID),		// output  Branch_sig
 	.MemRead(MemRead_ID),	// output  MemRead_sig
 	.MemtoReg(MemtoReg_ID),	// output  MemtoReg_sig
-	.ALUOp(AluOp_ID),	// output [1:0] ALUOp_sig
+	.ALUOp(AluOp_ID),			// output [1:0] ALUOp_sig
 	.MemWrite(MemWrite_ID),	// output  MemWrite_sig
-	.ALUSrc(AluSrc_ID),	// output  ALUSrc_sig
-	.RegWrite(RegWrite_ID), 	// output  RegWrite_sig
-	.AuipcLui(AuipcLui_ID),
-	.Jal(Jal_ID)
+	.ALUSrc(AluSrc_ID),		// output  ALUSrc_sig
+	.RegWrite(RegWrite_ID),	// output  RegWrite_sig
+	.AuipcLui(AuipcLui_ID),	// output [1:0] AuipcLui_sig
+	.Jal(Jal_ID),
+	.Jalr(Jalr_ID) //******************REVISAR******************
 );
 
-//ID/EX
+//------------ID/EX------------
 always @(posedge CLK)
 	RegWrite_EX = RegWrite_ID;
 	MemtoReg_EX = MemtoReg_ID;
@@ -80,48 +84,81 @@ always @(posedge CLK)
 	PC_EX = PC_ID;
 	
 	readData1_EX = readData1_ID;
-	readData1_EX = readData2_ID;
+	readData2_EX = readData2_ID;
 	
 	inm_out_EX = inm_out_ID;
 	
 	instr_EX = instr_ID;
 
 
-// Mux 3 a 1 para entrada A de la ALU
+//Mux 3 a 1 para entrada A de la ALU
     always_comb begin
-        case (AuipcLui_sig)
-            2'b00: valor_A = PC_IF;        
+        case (AuipcLui_EX)
+            2'b00: valor_A = PC_EX;        
             2'b01: valor_A = 32'd0;     
-            2'b10: valor_A = regis_A;   
-            default: valor_A = regis_A;
+            2'b10: valor_A = readData1_EX;   
+            default: valor_A = readData1_EX;
         endcase
     end
+	 
 		
-		
+assign valor_B = (AluSrc_EX) ? inm_out_EX : readData2_EX; //mux que selecciona entrada B alu		
+
+
 ALU ALU_inst
 (
 	.A(valor_A),	// mux 3 a 1 
 	.B(valor_B),	// mux indica si ReadData2 o immGen, seleccion=ALUSrc
 	.ALU_control(ALU_operation),	// controlado por modulo alu control
-	.ALU_result(alu_out_ext),	// decision del salto
-	.zero(zero_sig) 	// output  zero_sig
+	.ALU_result(alu_out_ext_EX),	// decision del salto
+	.zero(zero_EX) 	// output  zero_sig
 );
+
+
+assign instruction_bits_sig = {instr_EX[30],instr_EX[14:12]};
 
 
 ALU_CONTROL ALU_CONTROL_inst
 (
-	.ALUOp(ALUOp_sig),
+	.ALUOp(AluOp_EX),
 	.instruction_bits(instruction_bits_sig),
 	.ALU_control(ALU_operation)
 );
 
 
-assign PCSrc = (zero_sig & Branch_sig) || Jal_sig; // Esta puerta AND es de 3 entradas zero_sig,  Branch_sig y Jal, si el jal esta activado activa el mux para que pase el PC_IF+4
-// La señal Jal y Jal R salen de Alu Control, hay que cambiar ese modulo para que genere esas señales. 
-assign PC_siguiente = (PCSrc) ? (PC_IF + inm_out) : (PC_IF + 4);
-assign valor_B = (ALUSrc_sig) ? inm_out : regis_B; //mux que selecciona entrada B alu
-assign ena_wr = MemWrite_sig;
-assign ena_rd = MemRead_sig;
-assign instruction_bits_sig = {instr_IF[30],instr_IF[14:12]};
-assign dataram_wr = regis_B;
+assign PC_inm = PC_EX + inm_out_EX;
+
+
+//------------EX/MEM------------
+always @(posedge CLK)
+	RegWrite_MEM = RegWrite_EX;
+	MemtoReg_MEM = MemtoReg_EX;
+	Branch_MEM = Branch_EX;
+	MemRead_MEM = MemRead_EX;
+	MemWrite_MEM = MemWrite_EX;
+	zero_MEM = zero_EX;
+	alu_out_ext_MEM = alu_out_ext_EX;
+	PC_MEM = PC_EX;
+	Jal_MEM = Jal_EX;
+	PC_inm_MEM = PC_siguiente;
+	instr_MEM = instr_EX;
+	readData2_MEM = readData2_EX;
+
+	
+assign PCSrc = (zero_MEM & Branch_MEM) || Jal_MEM;
+assign ena_wr = MemWrite_MEM;
+assign ena_rd = MemRead_MEM;
+assign dataram_wr = readData2_MEM;
+
+
+//------------MEM/WB------------
+always @(posedge CLK)
+	RegWrite_WB = RegWrite_MEM;
+	MemtoReg_WB = MemtoReg_MEM;
+	alu_out_ext_WB = alu_out_ext_MEM;
+	dataram_rd_WB = dataram_rd;
+
+
+assign datareg_wr = (MemtoReg_WB == 2'b01) ? dataram_rd_WB : ((MemtoReg_WB == 2'b00) ? alu_out_ext_WB : PC + 4); //Write data en banco de registros
+
 endmodule
