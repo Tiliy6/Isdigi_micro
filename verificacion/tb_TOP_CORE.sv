72a7f595a85a4c7b7d7358c11309048989b3df8d
ooucelm <oussamaouchtoubane12@gmail.com>
2025-12-18 11:05:03 +0100

    Update tb_TOP_CORE.sv

--- verificacion/tb_TOP_CORE.sv
+++ verificacion/tb_TOP_CORE.sv
@@ -2,30 +2,31 @@
 module tb_TOP_CORE();
 	localparam T = 20;
 	
-	logic [31:0] instr, datareg_wr, PC, alu_out_ext, dataram_wr;
+	logic [31:0] instr, datareg_wr, PC, alu_out_ext, dataram_wr, dataram_rd_sig;
 	logic CLOCK, RST_n, ena_wr, ena_rd, MemtoReg_sig;
 	
 	logic [31:0] [31:0] registro; //x0=registro[0],x1=registro[1], ect
-	logic [31:0] rs1, rs2, inm_ext_32; // Contenido de 32 bits
+	logic [31:0] rs1, rs2, inm_ext_32, expected_addr; // Contenido de 32 bits
 	logic [11:0] inm_orshamt, inm;
 	logic [2:0] func3;
 	logic [31:0] resultado_esperado;
 	logic true_false;
 	logic [19:0] inm_tipoU;
+	logic [4:0] rd_addr;
 	
-	TOP_CORE duv (
-		.instr(instr) ,
-		.datareg_wr(alu_out_ext) ,
-		.CLOCK(CLOCK) ,
-		.RST_n(RST_n) ,
-		.PC(PC) ,
-		.ena_wr(ena_wr) ,
-		.ena_rd(ena_rd) ,
-		.alu_out_ext(alu_out_ext) ,
-		.MemtoReg_sig(MemtoReg_sig) ,
-		.dataram_wr(dataram_wr) 
+	TOP_CORE duv
+	(
+		.instr(instr) ,	// input [31:0] instr_sig
+		.dataram_rd(alu_out_ext) ,	// input [31:0] datareg_wr_sig
+		.CLOCK(CLOCK) ,	// input  CLOCK_sig
+		.RST_n(RST_n) ,	// input  RST_n_sig
+		.PC(PC) ,	// output [31:0] PC_sig
+		.ena_wr(ena_wr) ,	// output  ena_wr_sig
+		.ena_rd(ena_rd) ,	// output  ena_rd_sig
+		.alu_out_ext(alu_out_ext) ,	// output [31:0] alu_out_ext_sig
+		.dataram_wr(dataram_wr) 	// output [31:0] dataram_wr_sig
 	);
-	
+
 	class instruccionRandom;
 		rand logic [31:0] instr;
 
@@ -49,6 +50,16 @@ module tb_TOP_CORE();
 		
 		constraint U_format {
 			instr[6:0] == 7'b0010111 || instr[6:0] == 7'b0110111; //AUIPC or LUI
+		}
+		
+		constraint carga_format {
+			instr[6:0] == 7'b0000011;
+			instr[14:12] == 3'b010;
+		}
+		
+		constraint S_format {
+			instr[6:0] == 7'b0100011;
+			instr[14:12] == 3'b010;
 		}
 
 	endclass
@@ -118,13 +129,37 @@ module tb_TOP_CORE();
 		cruceU: cross rd_cp, inm_cp;
 	endgroup;
 
+	covergroup carga_type;
+		inm_cp: coverpoint $signed({instr[31:20]}) {
+			bins val[8] = {[-2048:2047]};
+		}
+		rs1_cp: coverpoint instr[19:15] {
+			bins val[4] = {[0:31]};
+		}
+		rd_cp: coverpoint instr[11:7] {
+			bins val[] = {[0:31]};
+		}
+		cruce_carg: cross inm_cp, rs1_cp, rd_cp;
+	endgroup;
+	
+	covergroup S_type;
+		inm_cp: coverpoint $signed({instr[31:25],instr[11:7]}) {
+			bins val[] = {[-2048:2047]};
+		}
+		rs1_cp: coverpoint instr[19:15] {
+			bins val[4] = {[0:31]};
+		}
+		cruceS: cross inm_cp, rs1_cp;
+	endgroup;
+	
 	//Declaracion de objetos
 	instruccionRandom busInst = new;
 	R_type veamosR = new;
 	I_type veamosI = new;
 	B_type veamosB = new;
 	U_type veamosU = new;
-	
+	carga_type veamos_car = new;
+	S_type veamosS = new;
 	
 	//DEFINICION DEL CLOCK
 	always
@@ -151,7 +186,9 @@ task R_instructions;
 			busInst.I_format.constraint_mode(0);
 			busInst.B_format.constraint_mode(0);
 			busInst.U_format.constraint_mode(0);
-
+			busInst.carga_format.constraint_mode(0);
+			busInst.S_format.constraint_mode(0);
+			
 			assert(busInst.randomize()) else $error("Falló randomize()");
 			instr = busInst.instr; 
 			
@@ -194,6 +231,8 @@ task R_instructions;
 			busInst.I_format.constraint_mode(1);
 			busInst.B_format.constraint_mode(0);
 			busInst.U_format.constraint_mode(0);
+			busInst.carga_format.constraint_mode(0);
+			busInst.S_format.constraint_mode(0);
 			
 			assert(busInst.randomize()) else $error("Falló randomize()");
 			instr = busInst.instr;
@@ -233,6 +272,8 @@ task R_instructions;
 			busInst.I_format.constraint_mode(0);
 			busInst.B_format.constraint_mode(1);
 			busInst.U_format.constraint_mode(0);
+			busInst.carga_format.constraint_mode(0);
+			busInst.S_format.constraint_mode(0);
 			
 			assert(busInst.randomize()) else $error("Falló randomize()");
 			instr = busInst.instr;
@@ -270,6 +311,8 @@ task R_instructions;
 			busInst.I_format.constraint_mode(0);
 			busInst.B_format.constraint_mode(0);
 			busInst.U_format.constraint_mode(1);
+			busInst.carga_format.constraint_mode(0);
+			busInst.S_format.constraint_mode(0);
 			
 			assert(busInst.randomize()) else $error("Falló randomize()");
 			instr = busInst.instr;
@@ -283,6 +326,69 @@ task R_instructions;
 			@(posedge CLOCK)
 			assert (resultado_esperado == alu_out_ext) else $error("operacion tipo U mal realizada");
 			veamosU.sample();
+			
+		end
+	endtask
+	
+	task carga_instructions;
+		begin
+			busInst.R_format.constraint_mode(0);
+			busInst.I_format.constraint_mode(0);
+			busInst.B_format.constraint_mode(0);
+			busInst.U_format.constraint_mode(0);
+			busInst.carga_format.constraint_mode(1);
+			busInst.S_format.constraint_mode(0);
+			
+			assert(busInst.randomize()) else $error("Falló randomize()");
+			instr = busInst.instr;
+			
+			inm = instr[31:20];
+			rs1 = registro[instr[19:15]];
+			rd_addr = instr[11:7];
+			
+			expected_addr = rs1 + {{20{inm[11]}},inm}; //para comprobar que la direccion esperada coincide con la que usamos en memoria
+			
+			@(posedge CLOCK)
+			assert (expected_addr == alu_out_ext) else $error("la direccion en la operacion de carga no es correcta");
+			assert (duv.banco_registros_inst.RegWrite == 1'b1) else $error("el banco de registros no lee ninguna palabra");
+			assert (duv.CONTROL_inst.ALUSrc == 1'b1) else $error("el inmediato no se usa en el calculo de la direccion");
+			assert (ena_rd == 1'b1) else $error("no se lee ningun dato de la RAM");
+			assert (duv.banco_registros_inst.writeReg == rd_addr) else $error("el dato no se guarda en la direccion que corresponde");
+
+
+			veamos_car.sample();
+			
+		end
+	endtask
+
+	task S_instructions;
+		begin
+			busInst.R_format.constraint_mode(0);
+			busInst.I_format.constraint_mode(0);
+			busInst.B_format.constraint_mode(0);
+			busInst.U_format.constraint_mode(0);
+			busInst.carga_format.constraint_mode(0);
+			busInst.S_format.constraint_mode(1);
+			
+			assert(busInst.randomize()) else $error("Falló randomize()");
+			instr = busInst.instr;
+			
+			inm = {instr[31:25],instr[11:7]};
+			rs1 = registro[instr[19:15]];
+			rs2 = registro[instr[24:20]]; //dato que queremos escribir en mem 
+			
+			expected_addr = rs1 + {{20{inm[11]}},inm}; //para comprobar que la direccion esperada coincide con la que usamos en memoria
+			
+			@(posedge CLOCK)
+			assert (expected_addr == alu_out_ext) else $error("la direccion en la operacion de store word no es correcta");
+			assert (duv.banco_registros_inst.RegWrite == 1'b0) else $error("el banco de registros no debe leer ninguna palabra");
+			assert (duv.CONTROL_inst.ALUSrc == 1'b1) else $error("el inmediato no se usa en el calculo de la direccion");
+			assert (ena_wr == 1'b1) else $error("no se habilita la escritura en la RAM");
+			assert (duv.banco_registros_inst.readData2 == rs2) else $error("el dato que se guarda no coincide con el del registro (objetivo)");
+		
+
+
+			veamosS.sample();
 			
 		end
 	endtask
@@ -346,6 +452,26 @@ task R_instructions;
 				 init_registros();
 				 #2
 				 U_instructions;
+			 end
+			 
+		 @(negedge CLOCK);
+		 while (veamos_car.cruce_carg.get_coverage() < 50)
+			
+			 begin
+				 @(posedge CLOCK)
+				 init_registros();
+				 #2
+				 carga_instructions;
+			 end
+			 
+		 @(negedge CLOCK);
+		 while (veamosS.cruceS.get_coverage() < 50)
+			
+			 begin
+				 @(posedge CLOCK)
+				 init_registros();
+				 #2
+				 S_instructions;
 			 end
 			 
 		$display("Test finished");
