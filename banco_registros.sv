ee8a32d3ed4f6170d328bfc277ca0acb2281ceaf
remonsot <remonsot@teleco.upv.es>
2025-12-19 13:59:50 +0100

    Update banco_registros.sv

--- banco_registros.sv
+++ banco_registros.sv
@@ -1,25 +1,25 @@
-module banco_registros(CLK, RST_n,readReg1, readReg2, writeReg, writeData, readData1, readData2, RegWrite);
-input [4:0] readReg1, readReg2, writeReg;
-input CLK, RST_n, RegWrite; //RegWrite enable de escritura
-input [31:0] writeData;
-output logic [31:0] readData1, readData2;
-logic [31:0] [31:0] registro; //x0=registro[0],x1=registro[1], ect
+module banco_registros(
+    input  logic        CLK, RST_n,
+    input  logic [4:0]  readReg1, readReg2, writeReg,
+    input  logic [31:0] writeData,
+    input  logic        RegWrite,
+    output logic [31:0] readData1, readData2
+);
 
+logic [31:0] registro [31:0]; // 32 registros de 32 bits
 
-always_ff @(posedge CLK or negedge RST_n)
-	begin
-		if (!RST_n)
-			registro <= 0;
-		else
-			registro[0] <= 0;
-			if(RegWrite && (writeReg != 5'd0))
-			registro[writeReg] <= writeData;
-	end
-	
-always_comb
-	readData1 = registro[readReg1];
-	
-always_comb
-	readData2 = registro[readReg2];
+always_ff @(posedge CLK or negedge RST_n) begin
+    if (!RST_n) begin
+        registro <= '{default:0};   // reset a cero
+    end else begin
+        registro[0] <= 32'b0;       // x0 siempre vale 0
+        if (RegWrite && writeReg != 5'd0)
+            registro[writeReg] <= writeData;
+    end
+end
+
+assign readData1 = registro[readReg1];
+assign readData2 = registro[readReg2];
 
 endmodule
+
